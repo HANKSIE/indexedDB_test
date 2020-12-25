bf8f3f8c581795a634a64aa98e0af9773b357d8f
HANKSIE <32055793+HANKSIE@users.noreply.github.com>
2020-12-26 03:19:58 +0800

    build indexeddb query builder

--- index.js
+++ index.js
@@ -6,15 +6,17 @@ dbRequest.onerror = function (event) {
   alert("Database error");
 };
 
-dbRequest.onsuccess = function (event) {
+dbRequest.onsuccess = async function (event) {
   DB = dbRequest.result;
 
+  console.log("success");
   DB.onerror = function (event) {
     alert("Database error: " + event.target.errorCode);
   };
 
   //transaction here
-  seedRun();
+  // removeAll();
+  // seedRun();
 };
 
 dbRequest.onupgradeneeded = function (event) {
@@ -31,130 +33,158 @@ dbRequest.onupgradeneeded = function (event) {
 
 function getObjectStore(store_name, mode) {
   var transaction = DB.transaction(store_name, mode);
-  transaction.oncomplete = function (event) {
-    console.log("done");
-  };
 
   transaction.onabort = function (event) {
-    console.error("action aborted..");
+    console.error("transaction aborted..");
   };
 
   transaction.onerror = function (event) {
-    console.error("action error..");
+    console.error("transaction error..");
   };
   return transaction.objectStore(store_name);
 }
 
 function find(id) {
-  var store = getObjectStore(STORE_NAME, "readonly");
+  return new Promise(function(resolve, reject){
+    var store = getObjectStore(STORE_NAME, "readonly");
 
-  var action = store.get(id);
-
-  action.onsuccess = function (event) {
-    console.log(action.result);
-  };
-
-  action.onerror = function (event) {
-    console.error("read fail");
-  };
+    var action = store.get(id);
+  
+    action.onsuccess = function () {
+      resolve(action.result);
+    };
+  
+    action.onerror = function () {
+      reject(false);
+    };
+  });
 }
 
 function count() {
-  var store = getObjectStore(STORE_NAME, "readonly");
+  return new Promise(function(resolve, reject){
+    var store = getObjectStore(STORE_NAME, "readonly");
 
-  var action = store.count();
-
-  action.onsuccess = function (event) {
-    console.log(action.result);
-  };
-
-  action.onerror = function (event) {
-    console.error("read fail");
-  };
+    var action = store.count();
+  
+    action.onsuccess = function () {
+      resolve(action.result);
+    };
+  
+    action.onerror = function () {
+      reject(new Error("count fail"));
+    };
+  }) 
 }
 
 function add(data) {
-  var store = getObjectStore(STORE_NAME, "readwrite");
+  return new Promise(function(resolve, reject){
+    var store = getObjectStore(STORE_NAME, "readwrite");
 
-  var action = store.add(data);
-
-  action.onsuccess = function (event) {
-    console.log(action.result);
-  };
-
-  action.onerror = function (event) {
-    console.error("read fail");
-  };
+    var action = store.add(data);
+  
+    action.onsuccess = function (event) {
+      resolve(true);
+    };
+  
+    action.onerror = function (event) {
+      reject(false);
+    };
+  })
+ 
 }
 
 function remove(id) {
-  var store = getObjectStore(STORE_NAME, "readwrite");
+  return new Promise(function(resolve, reject){
+    var store = getObjectStore(STORE_NAME, "readwrite");
 
-  var action = store.delete(id);
-
-  action.onsuccess = function (event) {
-    console.log(action.result);
-  };
-
-  action.onerror = function (event) {
-    console.error("read fail");
-  };
+    var action = store.delete(id);
+  
+    action.onsuccess = function (event) {
+      resolve(true);
+    };
+  
+    action.onerror = function (event) {
+      reject(false);
+    };
+  });
+ 
 }
 
 function removeAll() {
-  var store = getObjectStore(STORE_NAME, "readwrite");
+  return new Promise(function(resolve, reject){
+    var store = getObjectStore(STORE_NAME, "readwrite");
 
-  var action = store.clear();
-
-  action.onsuccess = function (event) {
-    console.log(action.result);
-  };
-
-  action.onerror = function (event) {
-    console.error("read fail");
-  };
+    var action = store.clear();
+  
+    action.onsuccess = function () {
+      resolve(true);
+    };
+  
+    action.onerror = function () {
+      reject(false);
+    };
+  });
+ 
 }
 
-function readAll() {
-  var store = getObjectStore(STORE_NAME, "readonly");
+function getAll() {
+  var collection = [];
+  return new Promise(function(resolve, reject){
+    var store = getObjectStore(STORE_NAME, "readonly");
 
-  store.openCursor().onsuccess = function (event) {
-    var cursor = event.target.result;
-
-    if (cursor) {
-      console.log(cursor.value);
-      cursor.continue();
-    }
-  };
+    store.openCursor().onsuccess = function (event) {
+      var cursor = event.target.result;
+  
+      if (cursor) {
+        collection.push(cursor.value)
+        cursor.continue();
+      }else{
+        resolve(collection);
+      }
+    };
+  });
 }
 
 function search(field, target) {
-  var store = getObjectStore(STORE_NAME, "readonly");
-  var index = store.index(field);
+  var keys = [];
+  return new Promise(function(resolve){
+    var store = getObjectStore(STORE_NAME, "readonly");
+    var index = store.index(field);
 
-  index.openKeyCursor().onsuccess = function (event) {
-    var cursor = event.target.result;
+    index.openKeyCursor().onsuccess = async function (event) {
+      var cursor = event.target.result;
 
-    if (cursor) {
-      if (cursor.key.toString().includes(target)) {
-        console.log(find(cursor.primaryKey));
+      if (cursor) {
+        if (cursor.key.toString().includes(target)) {
+          keys.push(cursor.primaryKey);
+        }
+        cursor.continue();
+      }else{
+        var collection = [];
+        keys.forEach(async function(id){
+          collection.push(await find(id));
+        })
+        resolve(collection);
       }
-      cursor.continue();
-    }
-  };
+    };
+  });
 }
 
 function seedRun() {
-  var data = [
-    { name: "hank hsieh", age: 22 },
-    { name: "gary dou", age: 22 },
-    { name: "tshou chen lee", age: 22 },
-    { name: "stephen curry", age: 30 },
-    { name: "klay thompson", age: 33 },
-  ];
-  //transaction here
-  // removeAll();
-  data.forEach((item) => {
-    add(item);
+  return new Promise( function(resolve, reject){
+    var data = [
+      { name: "hank hsieh", age: 22 },
+      { name: "gary dou", age: 22 },
+      { name: "tshou chen lee", age: 22 },
+      { name: "stephen curry", age: 30 },
+      { name: "klay thompson", age: 33 },
+    ];
+
+    data.forEach(async (item) => {
+      await add(item);
+    });
+
+    resolve(true);
   });
+ 
 }
